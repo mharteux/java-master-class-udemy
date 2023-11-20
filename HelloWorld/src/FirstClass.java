e74e95939c8cb8e51303115404218ae339d0ecf9
Matheus Reynaud <matheuxreynaud@gmail.com>
2023-11-20 10:11:05 -0300

    java-master-class

---
+++ HelloWorld/src/FirstClass.java
@@ -0,0 +1,14 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+/*
+public = access modifier = Allow us to define of our code, or even someone else's code,
+can access a particular element.
+
+class = The class name will be the text following the keyword, so FirstClass in this case.
+
+left and right curly braces = Define the class code block, or class body.
+*/
