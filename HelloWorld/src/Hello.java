31f8960edc3427fdf100055af277576c27dfe293
Matheus Reynaud <matheuxreynaud@gmail.com>
2023-11-21 09:19:33 -0300

    java-master-class

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -10,7 +10,7 @@ left and right curly braces = Define the class code block, or class body.
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.print("Hello, Matheus");
+        System.out.println("Hello, Matheus");
 
         //if - then - Statement
         boolean isAlien = false;
@@ -44,6 +44,41 @@ public class Hello {
         boolean isCar = false;
         if (!isCar) {
             System.out.println("This is not supposed to happen");
+        }
+
+        /* ternary operator -> structure:
+        operand1 ? operand2 : operand3 */
+        String makeOfCar = "Volkswagen";
+        boolean isDomestic = makeOfCar == "Volkswagen" ? false : true;
+
+        if (isDomestic) {
+            System.out.println("This car is domestic to our country");
+        }
+
+        String s = (isDomestic) ? "This car is domestic" : "This car is not domestic";
+        System.out.println(s);
+
+        // operator precedence and operator challenge
+        // step 1: create a double variable with a value of 20.00
+        double myFirstValue = 20.00d;
+        // step 2: create a second variable of type double with a value 80.00
+        double mySecondValue = 80.00d;
+        // step 3: add both numbers together, then multiply by 100.00
+        double myValuesTotal = (myFirstValue + mySecondValue) * 100.00d;
+        System.out.println("MyValuesTotal = " + myValuesTotal);
+        /* step 4: use the remainder operator, to figure out what the remainder from the
+        result of the operation in step three, and 40.00, will be */
+        double theRemainder = myValuesTotal % 40.00d;
+        System.out.println("theRemainder = " + theRemainder);
+        /* step 5: create a boolean variable that assigns the value true, if the remainder in
+        step four is 0.00 false if it's not zero */
+        boolean isNoRemainder = (theRemainder == 0) ? true : false;
+        // step 6: output the boolean variable just to see what the result is
+        System.out.println("isNoRemainder = " + isNoRemainder);
+        /* step 7: write an if-then statement that displays a message, 'got some remainder',
+        if the boolean in step five is not true */
+        if (!isNoRemainder) {
+            System.out.println("Got some remainder");
         }
     }
 }
