66368b1afd7c4084eec9daf7a6c1f4c137d6f764
Matheus Reynaud <matheuxreynaud@gmail.com>
2023-11-23 09:17:55 -0300

    java-master-class

--- IfKeywordAndCodeBlocks/src/MainChallenge.java
+++ IfKeywordAndCodeBlocks/src/MainChallenge.java
@@ -7,36 +7,22 @@ public class MainChallenge {
         int levelCompleted = 5;
         int bonus = 100;
 
-        int finalScore = score;
-
-        if (gameOver) {
-            finalScore += (levelCompleted * bonus);
-            finalScore += 1000;
-            System.out.println("Your final score was " + finalScore);
-        }
-
-/*
-        boolean newGameOver = true;
-        int newScore = 10000;
-        int newLevelCompleted = 8;
-        int newBonus = 200;
-
-        int newFinalScore = newScore;
-
-        if (newGameOver == true) {
-            newFinalScore += (newLevelCompleted * newBonus);
-            System.out.println("Your final score was " + newFinalScore);
-        }
-*/
+        calculateScore(gameOver, score, levelCompleted, bonus);
 
         score = 10000;
         levelCompleted = 8;
         bonus = 200;
 
-        finalScore = score;
+        calculateScore(gameOver, score, levelCompleted, bonus);
+    }
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+        int finalScore = score;
 
         if (gameOver) {
             finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
             System.out.println("Your final score was " + finalScore);
         }
     }
