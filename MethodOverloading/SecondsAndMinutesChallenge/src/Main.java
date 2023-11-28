64de0d0fcae7789d6d5b3ee1608e87cb7d1052a7
Matheus Reynaud <matheuxreynaud@gmail.com>
2023-11-28 10:43:38 -0300

    java-master-class

--- MethodOverloading/SecondsAndMinutesChallenge/src/Main.java
+++ MethodOverloading/SecondsAndMinutesChallenge/src/Main.java
@@ -2,24 +2,36 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(getDurationString(3945)); // This is the first test case
-        System.out.println(getDurationString(65, 45)); // This is the second test case
+        System.out.println(getDurationString(-3945)); // This is the first test case
+        System.out.println(getDurationString(-65, 45)); // This is the second test case
+        System.out.println(getDurationString(65, 145));
+        System.out.println(getDurationString(65,45));
+        System.out.println(getDurationString(3945));
     }
 
     public static String getDurationString(int seconds) {
 
-        int minutes = seconds / 60;
-        return getDurationString(minutes, seconds);
+        if(seconds < 0) {
+            return "Invalid data for seconds(" + seconds + "), must be a positive integer value";
+        }
+
+        return getDurationString(seconds / 60, seconds % 60);
     }
 
     public static String getDurationString(int minutes, int seconds) {
 
+        if(minutes < 0) {
+            return "Invalid data for minutes(" + minutes + "), must be a positive integer value";
+        }
+
+        if(seconds <= 0 || seconds >= 59){
+            return "Invalid data for seconds(" + seconds + "), must be between 0 and 59";
+        }
+
         int hours = minutes / 60;
 
         int remainingMinutes = minutes % 60;
-        int remainingSeconds = seconds % 60;
 
-        return hours + "h " + remainingMinutes + "m " + remainingSeconds + "s";
-
+        return hours + "h " + remainingMinutes + "m " + seconds + "s";
     }
 }
